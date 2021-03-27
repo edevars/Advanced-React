bcfac573b7934fd309bde2317fe4df7ccd2d28f6
edevars <e.devars.dev@gmail.com>
2021-03-27 15:12:04 -0600

    :sparkles: Creating nav and header component

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 const Page = ({ children }) => (
   <>
+    <Header />
     <h1>Hey! I'm the page component</h1>
     {children}
   </>
