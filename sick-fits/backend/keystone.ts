7e3a7f796e53dd3e619f008fdc6872b8f2e69738
edevars <e.devars.dev@gmail.com>
2021-05-08 18:08:05 -0500

    :sparkles: Setting keystone server config

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import { createSchema, config } from "@keystone-next/keystone/schema";
+const databseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: "mongoose",
+    url: databseURL,
+  },
+  lists: createSchema({
+    // Schema items
+  }),
+  ui: {
+    // TODO: Change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
