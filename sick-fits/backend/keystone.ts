58ed6f9c0f32ffa2f05fa141a03d1bb81f34acbc
edevars <e.devars.dev@gmail.com>
2021-05-08 18:41:03 -0500

    :sparkles: Adding auth with sessions

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,12 @@
 import { User } from "./schemas/User";
+import { createAuth } from "@keystone-next/auth";
 import "dotenv/config";
 import { createSchema, config } from "@keystone-next/keystone/schema";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+
 const databseURL = process.env.DATABASE_URL;
 
 const sessionConfig = {
@@ -8,24 +14,40 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
   },
-  db: {
-    adapter: "mongoose",
-    url: databseURL,
-  },
-  lists: createSchema({
-    // Schema items
-    User,
-  }),
-  ui: {
-    // TODO: Change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databseURL,
+    },
+    lists: createSchema({
+      // Schema items
+      User,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: "id",
+    }),
+  })
+);
